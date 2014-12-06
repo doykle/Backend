b4be8806ad7834dd80ddad59931208fcbc550759
Kevin Doyle <kdoyle@ucsc.edu>
2014-12-05 18:54:04 -0800

    changed to Naive Bayes classifier
    KNN was not working so I added some evaluative code to the training
    method. After doing that, I played around with different classifiers and
    found that Naive Bayes performs well with the data we have. If anyone
    actually reads this, note that this is not the code or classifier we are
    currently using in the project.

--- Analysis and Classification/Analysis/Code/Kevin_Code/Analysis.py
+++ Analysis and Classification/Analysis/Code/Kevin_Code/Analysis.py
@@ -31,89 +31,92 @@ import re
 
 #For machine classification
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn import cross_validation
+from sklearn import svm
 
 #To save the classifier
 import pickle
            
 def init():
-	#Check for proper number of arguments, die if necessary
-	#max args = 2 + number of valid options
-	max_args = 10
-	if len(sys.argv) < 2:
-		print "Usage: Analysis.py [-cfhpvVw] source_file"
-		print "Call with -h for help"
-		sys.exit(-1)
-	if len(sys.argv) > max_args:
-		print "Usage: Analysis.py [-cfhpvVw] source_file"
-		print "Call with -h for help"
-		sys.exit(-2)
+   #Check for proper number of arguments, die if necessary
+   #max args = 2 + number of valid options
+   max_args = 10
+   if len(sys.argv) < 2:
+      print "Usage: Analysis.py [-cfhpvVw] source_file"
+      print "Call with -h for help"
+      sys.exit(-1)
+   if len(sys.argv) > max_args:
+      print "Usage: Analysis.py [-cfhpvVw] source_file"
+      print "Call with -h for help"
+      sys.exit(-2)
 
-	#handle calling with only Analysis.py -h
-	if sys.argv[1] == '-h':
-		print_help()
-		exit()
+   #handle calling with only Analysis.py -h
+   if sys.argv[1] == '-h':
+      print_help()
+      exit()
 
-	#Check for options. Break the option area of argv into a list of 
-	#characters. Make a list of those that are valid, and a list of 
-	#those that aren't. Let the user know if they endered an invalid 
-	#option.
+   #Check for options. Break the option area of argv into a list of 
+   #characters. Make a list of those that are valid, and a list of 
+   #those that aren't. Let the user know if they endered an invalid 
+   #option.
 
-	#When adding options, be sure to add a description to the -h section
-	#below
-	valid_options = 'cdfhMpTvVw'
-	alleged_options = list(set(''.join(sys.argv[1:-1])))
-	options = [x for x in alleged_options if re.search(x, valid_options)]
-	non_options = [x for x in alleged_options if x not in options and x != '-']
+   #When adding options, be sure to add a description to the -h section
+   #below
+   valid_options = 'cdfhMpTvVw'
+   alleged_options = list(set(''.join(sys.argv[1:-1])))
+   options = [x for x in alleged_options if re.search(x, valid_options)]
+   non_options = [x for x in alleged_options if x not in options and x != '-']
 
-	for i in non_options:
-		print "Ignoring invalid option: \'%s\'" % (i)
+   for i in non_options:
+      print "Ignoring invalid option: \'%s\'" % (i)
 
-	return options
+   return options
 
 #consider making times and currents an associative array
 def import_and_trim():
 #	Times = []
-	Currents = []
-	amp_ids = [70, 66, 74, 62, 78, 58, 50, 14, 54]
+   Currents = []
+   amp_ids = [70, 66, 74, 62, 78, 58, 50, 14, 54]
 
-	#Try to open source file for reading                                    
-	filename = sys.argv[len(sys.argv) - 1]                                  
-	if os.path.isfile(filename):                                               
-		with open(filename) as f:
-			#Check the first element of the first line.
-			#if it's "sensor_id", it's the old format
-			#if it's "1", it's the new format
+   #Try to open source file for reading                                    
+   filename = sys.argv[len(sys.argv) - 1]                                  
+   if os.path.isfile(filename):                                               
+      with open(filename) as f:
+         #Check the first element of the first line.
+         #if it's "sensor_id", it's the old format
+         #if it's "1", it's the new format
 
-			line = f.readline().split(',')
+         line = f.readline().split(',')
 
-			#New format
-			if line[0] == '1':
-				if line[1] == 'I':
-					Currents.append(line[3])
-				for line in f:
-					line = line.split(',')
-					if line[1] == 'I':
-						Currents.append(line[2])
-			else:
-				for line in f:
-					line = re.split(',|\t', line)
-					if int(line[0]) in amp_ids:
-						Currents.append(line[1])
-	else:
-		print "Analysis: cannot open file:", filename
+         #New format
+         if line[0] == '1':
+            if line[1] == 'I':
+               Currents.append(line[3])
+            for line in f:
+               line = line.split(',')
+               if line[1] == 'I':
+                  Currents.append(line[2])
+         else:
+            for line in f:
+               line = re.split(',|\t', line)
+               if int(line[0]) in amp_ids:
+                  Currents.append(line[1])
+   else:
+      print "Analysis: cannot open file:", filename
 
-	#Convert time since Unix epoch to intervals in microseconds             
-	#Convert currents to milliamps                                          
+   #Convert time since Unix epoch to intervals in microseconds             
+   #Convert currents to milliamps                                          
 #	Times = [ int(x) - int(Times[0]) for x in Times ]                       
-	Currents = [ 27*float(x)/1000 for x in Currents ] 
+   Currents = [ 27*float(x)/1000 for x in Currents ] 
 
-	return Currents, Times
+   return Currents, Times
 
 def produce_blocklist():
-	blocklist = []
+   blocklist = []
 
-	data_length = len(Currents) #or Times, just to de-specify
-	i = 0
+   data_length = len(Currents) #or Times, just to de-specify
+   i = 0
 #	if 'f' in Options:
 #		while i < data_length:
 #			block = []
@@ -129,83 +132,83 @@ def produce_blocklist():
 #			blocklist.append(block)
 #			i = j + 1
 #	else:
-	while i < data_length:
-		if i + 200 > len(Currents):                                       
-			break   
-		blocklist.append(Currents[i:i+blockwidth])                        
-		i += blockwidth 
+   while i < data_length:
+      if i + 200 > len(Currents):                                       
+         break   
+      blocklist.append(Currents[i:i+blockwidth])                        
+      i += blockwidth 
 
-	return blocklist
+   return blocklist
 
 
 def produce_mean_normalized_power_spectrum(blocklist):
-	#question: mean then normalize, or normalized then mean?
-	#doesn't matter because multiplication commutes
+   #question: mean then normalize, or normalized then mean?
+   #doesn't matter because multiplication commutes
 
-	#units are in square amps
-	#here's why:
-	#original was in amps
-	#take ft (not fft)
-	#break up original into frequency components
-	#view from frequency perspective
-	#frequency components are in amps
-	#take mod square using absolute
-	#mod square(a + bi) = a^2 + b^2
-	#units are now in square amps per hz
-	#integrate it in piecese from a to a + blockwidth
-	#now you have units of amps sqared
-	#put those in an array
-	#oh yeah
-	#so now we have a power spectrum for an amp signal (odd sounding)
-	#in amps squared
+   #units are in square amps
+   #here's why:
+   #original was in amps
+   #take ft (not fft)
+   #break up original into frequency components
+   #view from frequency perspective
+   #frequency components are in amps
+   #take mod square using absolute
+   #mod square(a + bi) = a^2 + b^2
+   #units are now in square amps per hz
+   #integrate it in piecese from a to a + blockwidth
+   #now you have units of amps sqared
+   #put those in an array
+   #oh yeah
+   #so now we have a power spectrum for an amp signal (odd sounding)
+   #in amps squared
 
-	#Produce power spectrum, sum                                 
-	power_spectrum = np.square(np.absolute(np.fft.rfft(blocklist[0])))
-	sum_power_spectrum = power_spectrum
-	for i in xrange(1, len(blocklist)):
-		power_spectrum = np.square(np.absolute(np.fft.rfft(blocklist[i])))
-		sum_power_spectrum = np.add(sum_power_spectrum, power_spectrum)
+   #Produce power spectrum, sum                                 
+   power_spectrum = np.square(np.absolute(np.fft.rfft(blocklist[0])))
+   sum_power_spectrum = power_spectrum
+   for i in xrange(1, len(blocklist)):
+      power_spectrum = np.square(np.absolute(np.fft.rfft(blocklist[i])))
+      sum_power_spectrum = np.add(sum_power_spectrum, power_spectrum)
 
-	#Take integral
-	total_sq_amps = 0.0
-	for i in xrange(0, len(sum_power_spectrum)):
-		total_sq_amps += sum_power_spectrum[i]
+   #Take integral
+   total_sq_amps = 0.0
+   for i in xrange(0, len(sum_power_spectrum)):
+      total_sq_amps += sum_power_spectrum[i]
 
-	#Normalize by total amps measured
-	normalized_power_spectrum = sum_power_spectrum * (1/total_sq_amps)
+   #Normalize by total amps measured
+   normalized_power_spectrum = sum_power_spectrum * (1/total_sq_amps)
 
-	#Finish taking mean
-	mean_normalized_spectrum = normalized_power_spectrum / len(blocklist)
+   #Finish taking mean
+   mean_normalized_spectrum = normalized_power_spectrum / len(blocklist)
 
-	return mean_normalized_spectrum
+   return mean_normalized_spectrum
 
 def display(spectrum):
-	template = np.ones(len(spectrum))
+   template = np.ones(len(spectrum))
 
-	#Get the plot ready and label the axes
-	pyp.plot(spectrum)
-	max_range = int(math.ceil(np.amax(spectrum) / standard_deviation))
-	for i in xrange(0, max_range):
-		pyp.plot(template * (mean + i * standard_deviation))
-	pyp.xlabel('Units?')
-	pyp.ylabel('Amps Squared')    
-	pyp.title('Mean Normalized Power Spectrum')
-	if 'V' in Options:
-		pyp.show()
-	if 'v' in Options:
-		tokens = sys.argv[-1].split('.')
-		filename = tokens[0] + ".png"
-		input = ''
-		if os.path.isfile(filename):
-			input = raw_input("Error: Plot file already exists! Overwrite? (y/n)\n")
-			while input != 'y' and input != 'n':
-				input = raw_input("Please enter either \'y\' or \'n\'.\n")
-			if input == 'y':
-				pyp.savefig(filename) 
-			else:
-				print "Plot not written."
-		else:
-			pyp.savefig(filename) 
+   #Get the plot ready and label the axes
+   pyp.plot(spectrum)
+   max_range = int(math.ceil(np.amax(spectrum) / standard_deviation))
+   for i in xrange(0, max_range):
+      pyp.plot(template * (mean + i * standard_deviation))
+   pyp.xlabel('Units?')
+   pyp.ylabel('Amps Squared')    
+   pyp.title('Mean Normalized Power Spectrum')
+   if 'V' in Options:
+      pyp.show()
+   if 'v' in Options:
+      tokens = sys.argv[-1].split('.')
+      filename = tokens[0] + ".png"
+      input = ''
+      if os.path.isfile(filename):
+         input = raw_input("Error: Plot file already exists! Overwrite? (y/n)\n")
+         while input != 'y' and input != 'n':
+            input = raw_input("Please enter either \'y\' or \'n\'.\n")
+         if input == 'y':
+            pyp.savefig(filename) 
+         else:
+            print "Plot not written."
+      else:
+         pyp.savefig(filename) 
 
 def write_output():
    tokens = sys.argv[-1].split('.')
@@ -233,58 +236,63 @@ def write_output():
    pkl_out.close()
 
 def print_help():
-	print "\nAnalysis.py."
-	print "Used to produce a mean power spectrum of raw SEAD plug current data, normalized by total current"
-	print "\nCall using this syntax:"
-	print "$ python Analysis.py [-fhpvVw] [input file]"
-	print ""
-	print "Input: Raw, 4-column SEAD plug data"
-	print "Output: Either or both of:"
-	print "1. Numpy array printed to stdout."
-	print "2. Spectrum written to text file."
-	print "\nOptions may be arranged in any order, and in any number of groups"
-	print ""
-	print "-f:\t Fragmented data. This handles gaps in the data."
-	print "-h:\t Help. Display this message."
-	print "-p:\t Print. Print numpy array containing spectrum to terminal."
-	print "-V:\t View. Display plot of spectrum, with the mean and multiples of the standard deviation."
-	print "-v:\t Visualize. Print plot to file."
-	print "-w:\t Write. Write spectrum to file, each array element on its own line"
+   print "\nAnalysis.py."
+   print "Used to produce a mean power spectrum of raw SEAD plug current data, normalized by total current"
+   print "\nCall using this syntax:"
+   print "$ python Analysis.py [-fhpvVw] [input file]"
+   print ""
+   print "Input: Raw, 4-column SEAD plug data"
+   print "Output: Either or both of:"
+   print "1. Numpy array printed to stdout."
+   print "2. Spectrum written to text file."
+   print "\nOptions may be arranged in any order, and in any number of groups"
+   print ""
+   print "-f:\t Fragmented data. This handles gaps in the data."
+   print "-h:\t Help. Display this message."
+   print "-M:\t Match. Matches the input data with a known pattern, using the classifier."
+   print "-p:\t Print. Print numpy array containing spectrum to terminal."
+   print "-T:\t Train. Trains the classifier using .Spectrum files in ./training_data. Please see the example use of the call."
+   print "-V:\t View. Display plot of spectrum, with the mean and multiples of the standard deviation."
+   print "-v:\t Visualize. Print plot to file."
+   print "-w:\t Write. Write spectrum to file, each array element on its own line. Also creates a .Spectrum file"
 
-	print "\nExamples:"
-	print "1: Handle fragmented data, view plot, write spectrum to file"
-	print "   python Analysis.py -vfw 1_raw.csv"
-	print "   python Analysis.py -f -wv 1_raw.csv"
-	print "2: View plot of spectrum"
-	print "   python Analysis.py -V 1_raw.csv"
+   print "\nExamples:"
+   print "1: Handle fragmented data, view plot, write spectrum to file"
+   print "   python Analysis.py -vfw 1_raw.csv"
+   print "   python Analysis.py -f -wv 1_raw.csv"
+   print "2: View plot of spectrum"
+   print "   python Analysis.py -V 1_raw.csv"
+   print "3: Training the classifier"
+   print "   python Analysis.py -T none"
+   print "                         ^ writing 'none' here is required"
 
 def classify(spectrum):
-	variation_coefficient = standard_deviation / mean
+   variation_coefficient = standard_deviation / mean
 
-	#count regions
-	count = 0
-	flag = 0
-	threshold = mean + standard_deviation
-	for i in xrange(0, len(spectrum)):
-		if flag == 0 and spectrum[i] > threshold:
-			flag = 1
-			count += 1
-			continue
-		elif flag == 1 and spectrum[i] < threshold:
-			flag = 0
+   #count regions
+   count = 0
+   flag = 0
+   threshold = mean + standard_deviation
+   for i in xrange(0, len(spectrum)):
+      if flag == 0 and spectrum[i] > threshold:
+         flag = 1
+         count += 1
+         continue
+      elif flag == 1 and spectrum[i] < threshold:
+         flag = 0
 
-	output_string = sys.argv[-1]
-	if len(output_string) < 24:
-		output_string = output_string + "\t"
+   output_string = sys.argv[-1]
+   if len(output_string) < 24:
+      output_string = output_string + "\t"
 
-	if variation_coefficient < 3.511:
-		print "%s\t was recorded from a laptop computer." % (output_string)
-		return
-	elif np.amax(spectrum) < 0.01 or count > 1:
-		print "%s\t was recorded from a microwave." % (output_string)
-		return
-	else:
-		print "%s\t was recorded from a lamp." % (output_string)
+   if variation_coefficient < 3.511:
+      print "%s\t was recorded from a laptop computer." % (output_string)
+      return
+   elif np.amax(spectrum) < 0.01 or count > 1:
+      print "%s\t was recorded from a microwave." % (output_string)
+      return
+   else:
+      print "%s\t was recorded from a lamp." % (output_string)
 
 def train_classifier():
    dir = 'training_data/'
@@ -299,7 +307,6 @@ def train_classifier():
             pkl_file = open( os.path.join( subdir, file ), 'r' )
             spec = pickle.load( pkl_file )
             spec = spec.tolist()
-
             training_data.append( spec )
             
             # Assembles labels corresponding with feature set data
@@ -322,13 +329,24 @@ def train_classifier():
       exit()
    
    # Make the classifier (yay!)
-   # We will use K Nearest Neighbors (knn)
-   knn = KNeighborsClassifier( n_neighbors=3 )
-   knn.fit( training_data, labels )
+   # Using Naive Bayes
+   clf = GaussianNB()
+   clf.fit( training_data, labels )
+   
+   ### Cross Validation, include ALL data
+   #print(cross_validation.cross_val_score( clf, training_data, labels, cv=3 ))
+   ###
+   
+   ### Used to check accuracy, include ALL data
+   #X_train, X_test, y_train, y_test = cross_validation.train_test_split( training_data, labels, test_size=0.3, random_state=0)
+   #clf = GaussianNB()
+   #clf.fit( X_train, y_train )
+   #print( clf.score(X_test, y_test) )
+   ###
    
    # Save classifier
    pkl_file = open( 'knn_pickle.classifier', 'w' )
-   pickle.dump( knn, pkl_file )
+   pickle.dump( clf, pkl_file )
    pkl_file.close()
    
    
@@ -343,8 +361,6 @@ def machine_classify( Spectrum ):
 
    print( knn.predict( spec ) )
    
-         
-#Execution begins here
 
 if __name__ == '__main__':
 
